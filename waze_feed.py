5b8b377648aeb0488a2347b34344677a53458c53
kid4rm90s <165776943+kid4rm90s@users.noreply.github.com>
2026-07-03 23:21:35 +0800

    1.0.1
    nepali date

--- waze_feed.py
+++ waze_feed.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import pytz
 import requests
 
-# version 1.0.0
+# version 1.0.1
 FEED_URL = "https://storage.googleapis.com/waze-tile-build-public/release-history/intl-feed.xml"
 WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 
