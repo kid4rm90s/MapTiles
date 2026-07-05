6e39cc0035f0738f92a332b3e990856eb4deba80
kid4rm90s <165776943+kid4rm90s@users.noreply.github.com>
2026-07-05 11:02:45 +0800

    run every 30 min
    run every 30 min

--- waze_feed.py
+++ waze_feed.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import pytz
 import requests
 
-# version 1.0.5
+# version 1.0.6
 FEED_URL = "https://storage.googleapis.com/waze-tile-build-public/release-history/intl-feed.xml"
 WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 STATE_FILE = "last_update.txt"
